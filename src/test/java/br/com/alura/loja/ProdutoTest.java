cbbea95626bd88db7200364bee3f3e1aa67e470f
Robson <robmartinhao@gmail.com>
2021-07-30 21:57:26 -0300

    Plugins - Jacoco

--- src/test/java/br/com/alura/loja/ProdutoTest.java
+++ src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,14 +1,18 @@
 package br.com.alura.loja;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		Produto p = new Produto("teste", BigDecimal.TEN);
+
+		Assert.assertEquals("teste", p.getNome());
+		Assert.assertEquals(BigDecimal.TEN, p.getPreco());
 	}
 
 }
